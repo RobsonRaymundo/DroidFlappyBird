5416e6222acbda135f408bd047575bf8ba9f18eb
Robson Raymundo <robson.raymundo@gmail.com>
2017-12-18 19:15:47 -0200

    finalizando o jogo

--- core/src/ray/droid/com/droidflappybird/FlappyBird.java
+++ core/src/ray/droid/com/droidflappybird/FlappyBird.java
@@ -2,32 +2,188 @@ package ray.droid.com.droidflappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+
+import java.util.Random;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture passaro;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		passaro = new Texture("passaro1.png");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(passaro, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		passaro.dispose();
-	}
+    private SpriteBatch batch;
+    private Texture[] passaros;
+    private Texture fundo;
+    private Texture canoBaixo;
+    private Texture canoTopo;
+    private Texture gameOver;
+    private Random numeroRandomico;
+
+    //Atributos de configuracao
+    private int larguraDispositivo;
+    private int alturaDispositivo;
+
+    private float variacao = 0;
+    private float velocidadeQueda = 0;
+    private float posicaoInicialVertical;
+    private float posicaoMovimentoCanoHorizontal;
+    private float espacoEntreCanos;
+    private float deltaTime;
+    private float alturaEntreCanosRandomica;
+    private int estadoJogo = 0;
+    private int pontuacao = 0;
+    private BitmapFont fonte;
+    private BitmapFont mensagem;
+    private boolean marcouPonto = false;
+    private Circle passaroCirculo;
+    private Rectangle retanguloCanoTopo;
+    private Rectangle retanguloCanoBaixo;
+ //   private ShapeRenderer shape;
+
+
+    @Override
+    public void create() {
+
+        batch = new SpriteBatch();
+        numeroRandomico = new Random();
+        passaroCirculo = new Circle();
+     //   retanguloCanoTopo = new Rectangle();
+     //   retanguloCanoBaixo = new Rectangle();
+    //    shape = new ShapeRenderer();
+        passaros = new Texture[3];
+        fonte = new BitmapFont();
+        fonte.setColor(Color.WHITE);
+        fonte.getData().setScale(6);
+
+        mensagem = new BitmapFont();
+        mensagem.setColor(Color.WHITE);
+        mensagem.getData().setScale(3);
+
+        passaros[0] = new Texture("passaro1.png");
+        passaros[1] = new Texture("passaro2.png");
+        passaros[2] = new Texture("passaro3.png");
+
+        fundo = new Texture("fundo.png");
+        canoBaixo = new Texture("cano_baixo.png");
+        canoTopo = new Texture("cano_topo.png");
+        gameOver = new Texture("game_over.png");
+
+        larguraDispositivo = Gdx.graphics.getWidth();
+        alturaDispositivo = Gdx.graphics.getHeight();
+        posicaoInicialVertical = alturaDispositivo / 2;
+        posicaoMovimentoCanoHorizontal = larguraDispositivo;
+        espacoEntreCanos = 300;
+
+    }
+
+    @Override
+    public void render() {
+        deltaTime = Gdx.graphics.getDeltaTime();
+        variacao += deltaTime * 10;
+        if (variacao > 2) variacao = 0;
+
+        if (estadoJogo == 0) { // jogo parado
+            if (Gdx.input.justTouched()) {
+                estadoJogo = 1;
+            }
+        } else {
+
+            velocidadeQueda++;
+            if (posicaoInicialVertical > 0 || velocidadeQueda < 0)
+                posicaoInicialVertical = posicaoInicialVertical - velocidadeQueda;
+
+            if (estadoJogo == 1) // jogo iniciado
+            {
+                posicaoMovimentoCanoHorizontal -= deltaTime * 200;
+                if (Gdx.input.justTouched()) {
+                    velocidadeQueda = -15;
+                }
+
+                //Verifica se o cano saiu inteiramente da tela
+                if (posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()) {
+                    posicaoMovimentoCanoHorizontal = larguraDispositivo;
+                    alturaEntreCanosRandomica = numeroRandomico.nextInt(400) - 200;
+                    marcouPonto = false;
+                }
+
+                if (posicaoMovimentoCanoHorizontal < 120) {
+                    if (!marcouPonto) {
+                        pontuacao++;
+                        marcouPonto = true;
+                    }
+                }
+            }
+            else // Game over
+            {
+                if (Gdx.input.justTouched())
+                {
+                    estadoJogo = 0;
+                    pontuacao = 0;
+                    velocidadeQueda =0;
+                    posicaoInicialVertical = alturaDispositivo / 2;
+                    posicaoMovimentoCanoHorizontal = larguraDispositivo;
+                }
+            }
+        }
+
+        batch.begin();
+
+        batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
+        batch.draw(canoTopo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 + espacoEntreCanos / 2 + alturaEntreCanosRandomica);
+        batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + alturaEntreCanosRandomica);
+        batch.draw(passaros[(int) variacao], 120, posicaoInicialVertical);
+        fonte.draw(batch, String.valueOf(pontuacao), larguraDispositivo / 2, alturaDispositivo - 50);
+
+        if (estadoJogo == 2)
+        {
+            batch.draw(gameOver, larguraDispositivo / 2 - gameOver.getWidth() / 2, alturaDispositivo / 2);
+            mensagem.draw(batch, "Toque para reiniciar", larguraDispositivo /2 - 200, alturaDispositivo / 2 - gameOver.getHeight() / 2);
+        }
+
+        batch.end();
+
+        passaroCirculo.set(120 + passaros[0].getWidth() / 2, posicaoInicialVertical + passaros[0].getHeight() / 2, passaros[0].getWidth() / 2);
+        retanguloCanoBaixo = new Rectangle(
+                posicaoMovimentoCanoHorizontal,
+                alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + alturaEntreCanosRandomica,
+                canoBaixo.getWidth(),
+                canoBaixo.getHeight()
+        );
+
+        retanguloCanoTopo = new Rectangle(
+                posicaoMovimentoCanoHorizontal,
+                alturaDispositivo / 2 + espacoEntreCanos / 2 + alturaEntreCanosRandomica,
+                canoTopo.getWidth(),
+                canoTopo.getHeight()
+        );
+
+        /*
+        //Desenhos das formas
+        shape.begin(ShapeRenderer.ShapeType.Filled);
+        shape.circle(passaroCirculo.x, passaroCirculo.y, passaroCirculo.radius);
+        shape.rect(retanguloCanoBaixo.x, retanguloCanoBaixo.y, retanguloCanoBaixo.width, retanguloCanoBaixo.height);
+        shape.rect(retanguloCanoTopo.x, retanguloCanoTopo.y, retanguloCanoTopo.width, retanguloCanoTopo.height);
+        shape.setColor(Color.RED);
+        shape.end();
+        */
+
+        // Teste de colisão
+        if (    Intersector.overlaps(passaroCirculo, retanguloCanoBaixo) ||
+                Intersector.overlaps(passaroCirculo, retanguloCanoTopo) ||
+                posicaoInicialVertical <=0 ||
+                posicaoInicialVertical >= alturaDispositivo) {
+            estadoJogo = 2;
+        }
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        passaros[0].dispose();
+        passaros[1].dispose();
+        passaros[2].dispose();
+    }
 }
